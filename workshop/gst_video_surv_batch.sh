b251a788b4540183f8fad09d7746fae62ed7a24a
Taras Lishchenko <jackersson@meta.ua>
2019-04-11 12:08:36 +0300

    :smile:

--- workshop/gst_video_surv_batch.sh
+++ workshop/gst_video_surv_batch.sh
@@ -2,7 +2,4 @@ rm -rf ~/.cache/gstreamer-1.0/
 # export GST_PLUGIN_PATH=gst-video-surveillance/core/gst
 
 cd gst-video-surveillance
-# python run.py -c single
 python run.py -c batch_double
-# python run.py -c triple
-# python run.py -c quadruple
